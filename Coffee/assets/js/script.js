ecde43df3aa49e0c09b3af1592815cfd764d6068
qulamali7 <147071333+qulamali7@users.noreply.github.com>
2023-11-14 10:40:29 +0400

    add

---
+++ Coffee/assets/js/script.js
@@ -0,0 +1,6 @@
+const btn_menuBar=document.querySelector(".btn-menuBar")
+const navMenu=document.querySelector(".navMenu")
+btn_menuBar.addEventListener("click",function() {
+    navMenu.classList.toggle("show")
+})
+
